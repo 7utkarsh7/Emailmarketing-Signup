4eaef022ebdd26e672993ab4d2c0b77ae2feca6f
7utkarsh7 <utkarshdohare2@gmail.com>
2020-09-15 14:51:31 +0530

    version 1.0

--- app.js
+++ app.js
@@ -67,6 +67,6 @@ app.post("/failure.html", function(req,res){
 
 
 
-app.listen( process.env.PORT||3000, function() {
+app.listen( process.env.PORT || 3000, function() {
   console.log("server running on port 3000");
 });
